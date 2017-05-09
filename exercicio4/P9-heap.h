72e7751f3814e624275f842d729fa3cbab62e62f
MateusSchulz <mateus.schulz.m@gmail.com>
2017-05-09 23:31:32 +0000

    Adicionando programa base da Questão04

---
+++ exercicio4/P9-heap.h
@@ -0,0 +1,21 @@
+/*	Include files for heap ADT
+	   Written by:
+	   Date:
+*/
+#include <stdlib.h>
+
+#include "P9-01.h"		// Heap Structure
+#include "P9-02.h"		// Create Heap
+#include "P9-03.h"		// Insert Heap
+#include "P9-04.h"		// Reheap Up
+#include "P9-05.h"		// Delete Heap
+#include "P9-06.h"		// Reheap Down
+
+#include "P9-06a.h"		// heap Count
+#include "P9-06b.h"		// heap full
+#include "P9-06c.h"		// heap empty
+
+//  Functions not developed in text
+//int  heapCount (HEAP* heap);
+//bool heapFull  (HEAP* heap);
+//bool heapEmpty (HEAP* heap);
