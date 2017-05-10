f8cdd816091d97a2946b2ce6839a36dfcd7aed72
MateusSchulz <mateus.schulz.m@gmail.com>
2017-05-10 00:47:17 +0000

    Alterando a Questão03

--- exercicio3/exercicio3.c
+++ exercicio3/exercicio3.c
@@ -4,18 +4,58 @@
 #include <stdbool.h>
 #include <time.h>
 
+int contador = 0;
 #include "P9-heap.h"
-const int maxSize = 15;
+const int maxSize = 100000000;
 
 //	Prototype Definitions 
 int  compare    (void* arguPtr, void* heapPtr);
 void printHeap (HEAP* heap, int idx, int recuo);
+HEAP* preencherHeap ();
 
 int main(void){
 //	Local Definitions 
+	HEAP *heap;
+	srand(time(NULL));
 
 //	Statements
+	heap = heapCreate (maxSize, compare);
+	if (!heap)
+	   printf("Can't create heap in main\n"), exit (100);
+
+	heap = preencherHeap();
+	printf("\nHEAP:\n");
+	//printHeap(heap, 0, 0);
+
+	printf("\n\nContador: %d, para n=%d\n", contador, heap->size);
+
+
 }	// main 
+
+HEAP* preencherHeap (){
+//	Local Definitions
+	int *dataPtr;
+	int bldLooper;
+
+//	Statements
+	HEAP* heap;
+	heap = heapCreate(maxSize, compare);
+	if(!heap)
+	   printf("Can't create heap in main\n"), exit (100);
+	printf("\nVETOR:\n");
+
+	for(bldLooper = 0; bldLooper < maxSize; bldLooper++){
+	    dataPtr = (int*)malloc(sizeof (int));
+	    if(!dataPtr)
+	        printf("Overflow in main\n"), exit (101);
+	    *dataPtr = rand() % 999 + 1;
+	    //printf("%d ", *dataPtr);
+	    heapInsert (heap, dataPtr);
+	    //printf("\n");
+	} // for 
+
+	return heap;
+}
 
 /*	================== compare ================= 
 	This function compares two integers identified
